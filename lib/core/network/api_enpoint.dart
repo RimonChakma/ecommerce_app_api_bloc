33aafc6af1f0e692f86eadb6a436ebd55c31c6d9
Rimon Chakma <rimon.chakma880@gmail.com>
2025-07-09 22:57:58 +0600

    add api enpoint class

---
+++ lib/core/network/api_enpoint.dart
@@ -0,0 +1,7 @@
+class ApiEndPoint{
+  static String baseUrl = "https://ecommerce-api.codesilicon.com/api";
+
+  static String getProduct ="/ListProductByRemark/popular";
+  static String getProductDetails = "/products";
+  static String getCategory = "/CategoryList";
+}
