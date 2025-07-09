9a60bfd4bc96af296544cc30ac984caa0fde869e
Rimon Chakma <rimon.chakma880@gmail.com>
2025-07-09 15:44:56 +0600

    add appColor

---
+++ lib/core/utils/constans..dart
@@ -0,0 +1,11 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class appColor {
+  static Color primaryColor = Color(0xFF00A79D);
+  static Color secondaryColor = Color(0xFF8BC34A);
+  static Color backgroundColor = Colors.white;
+  static Color textColor = Color(0xFF212121);
+  static Color lightGrey = Color(0xFFF2F2F2);
+  static Color dartGrey = Color(0xFF9E9E9E);
+}
