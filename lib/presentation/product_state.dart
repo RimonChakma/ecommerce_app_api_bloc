61a717e32f2db4ba730b117506be1a95c5f8b0b7
Rimon Chakma <rimon.chakma880@gmail.com>
2025-07-17 14:10:12 +0600

    add product state class

--- lib/presentation/product_state.dart
+++ lib/presentation/product_state.dart
@@ -1,5 +1,34 @@
-/// Created by USER PC on 7/15/2025
+import 'package:equatable/equatable.dart';
 
-void main() {
-  print('Hello, world!');
+import '../data/models/cetagories_model.dart';
+import '../data/models/product_model.dart';
+
+abstract class ProductState extends Equatable{
+  const ProductState();
+  @override
+  List<Object?> get props => [];
+}
+
+class ProductInitial extends ProductState{}
+class ProductLoading extends ProductState{}
+
+class ProductError extends ProductState{
+  final String message;
+  const ProductError(this.message);
+  @override
+  List<Object?> get props => [message];
+}
+
+class CategoryLoaded extends ProductState{
+  final List<Category> category;
+  const CategoryLoaded(this.category);
+  @override
+  List<Object?> get props => [category];
+}
+
+class PopularProduct extends ProductState{
+  final List<Product> product;
+  const PopularProduct(this.product);
+  @override
+  List<Object?> get props => [product];a
 }
